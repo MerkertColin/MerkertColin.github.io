b896aaede875fed01256eab47c14d502bc1f5e49
System Administrator <root@air-von-colin.speedport.ip>
2023-12-26 22:02:26 +0100

    Updates

--- _app/immutable/chunks/index.5WoYFeK4.js
+++
@@ -1,3 +0,0 @@
-import{i}from"./scheduler.BYpAmZKV.js";const s=400,g=s,P={duration:s,x:"-100vw"},T={duration:s,delay:g,x:"100vw"};function v(n){const t=n-1;return t*t*t+1}function U(n,{delay:t=0,duration:c=400,easing:e=v,x:u=0,y:f=0,opacity:y=0}={}){const o=getComputedStyle(n),a=+o.opacity,l=o.transform==="none"?"":o.transform,m=a*(1-y),[d,p]=i(u),[$,x]=i(f);return{delay:t,duration:c,easing:e,css:(r,_)=>`
-			transform: ${l} translate(${(1-r)*d}${p}, ${(1-r)*$}${x});
-			opacity: ${a-m*_}`}}export{U as f,T as i,P as o};

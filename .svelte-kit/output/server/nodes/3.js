7f73462171c0dd77989357d33d0cc766cdbfc470
System Administrator <root@air-von-colin.speedport.ip>
2023-12-27 11:19:18 +0100

    update

--- .svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/about/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.4KuWxYvv.js","_app/immutable/chunks/scheduler.GJjAvMcU.js","_app/immutable/chunks/index.-quLlP0Y.js","_app/immutable/chunks/each.-oqiv04n.js","_app/immutable/chunks/index.JpawHTMn.js"];
-export const stylesheets = ["_app/immutable/assets/3.Bi54jIOS.css"];
-export const fonts = [];
